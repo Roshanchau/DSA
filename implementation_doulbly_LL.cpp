92992d7f62df0c1e7877bfdf290e8faff9c3dca9
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-02-08 15:25:58 +0545

    deletion of doublyl LL

--- implementation_doulbly_LL.cpp
+++ implementation_doulbly_LL.cpp
@@ -15,6 +15,19 @@ public:
         this->prev = NULL;
         this->next = NULL;
     }
+
+    // destructor
+    ~Node()
+    {
+        int value = this->data;
+        // memory free
+        if (this->next != NULL)
+        {
+            delete next;
+            this->next = NULL;
+        }
+        cout << "memory is free for node with data " << value << endl;
+    }
 };
 
 void print(Node *&head)
@@ -40,26 +53,38 @@ int lengthOfLL(Node *&head)
     return len;
 }
 
-void insertAtHead(Node *&head, int a)
+void insertAtHead(Node *&head, Node *&tail, int a)
 {
-    Node *temp = new Node(a);
     if (head != NULL)
     {
+        Node *temp = new Node(a);
         temp->next = head;
         head->prev = temp;
         head = temp;
     }
     else
     {
-        return;
+        Node *temp = new Node(a);
+        head = temp;
+        tail = temp;
     }
 }
-void insertAtTail(Node *&tail, int a)
+void insertAtTail(Node *&tail, Node *&head, int a)
 {
-    Node *newNode = new Node(a);
-    tail->next = newNode;
-    newNode->prev = tail;
-    tail = newNode;
+
+    if (tail != NULL)
+    {
+        Node *newNode = new Node(a);
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+    else
+    {
+        Node *temp = new Node(a);
+        tail = temp;
+        head = temp;
+    }
 }
 
 void insertAtPos(Node *&tail, Node *&head, int pos, int z)
@@ -68,19 +93,22 @@ void insertAtPos(Node *&tail, Node *&head, int pos, int z)
 
     if (pos == 1)
     {
-        insertAtHead(head, z);
+        insertAtHead(head, tail, z);
         return;
     }
     int cnt = 1;
     while (cnt < pos - 1) //-> we have to acess the node before the curr node in the given pos.
     {
         temp = temp->next;
+        cout << temp->data << endl;
         cnt++; // increased the cnt by1;
+        cout << "count=" << cnt << endl;
     }
-
     if (temp->next == NULL)
     {
-        insertAtTail(tail, z);
+        cout << temp->next << endl;
+
+        insertAtTail(tail, head, z);
         return;
     }
     Node *nodeToInsert = new Node(z);
@@ -89,6 +117,38 @@ void insertAtPos(Node *&tail, Node *&head, int pos, int z)
     temp->next = nodeToInsert;
     nodeToInsert->prev = temp;
 }
+
+void deleteNode(int position, Node *&head)
+{
+    if (position == 1)
+    {
+        // deleting first node
+        Node *temp = head;
+        head = head->next;
+        // memory free for start node
+        temp->next = NULL;
+        temp->next->prev = NULL;
+        delete temp;
+    }
+    else
+    {
+        // deleting any node and last node
+        Node *prev;
+        Node *curr = head;
+        int cnt = 1;
+        while (cnt < position)
+        {
+            prev = curr;
+            curr = curr->next;
+            cnt++;
+        }
+        prev->next = curr->next;
+        curr->next->prev = prev;
+        curr->prev = NULL;
+        curr->next = NULL;
+        delete curr;
+    }
+}
 int main()
 {
     Node *node1 = new Node(10);
@@ -96,21 +156,25 @@ int main()
     Node *tail = node1;
     print(head);
 
-    insertAtHead(head, 11);
+    insertAtHead(head, tail, 11);
     print(head);
 
-    insertAtTail(tail, 20);
+    insertAtTail(tail, head, 20);
     print(head);
 
-    insertAtHead(head, 4);
+    insertAtHead(head, tail, 4);
     print(head);
 
-    insertAtTail(tail, 6);
+    insertAtTail(tail, head, 6);
     print(head);
 
-    insertAtPos(tail, head, 5, 33);
+    insertAtPos(tail, head, 6, 33);
     print(head);
 
+    deleteNode(2, head);
+    print(head);
+    deleteNode(4, head);
+    print(head);
     cout << lengthOfLL(head) << endl;
 
     return 0;
