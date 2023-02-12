da5a987e59f358cc78c91c64fe4c7feaf1dc1076
jfjsdlfjlsdkfj <roshanchau001@gmail.com>
2023-02-12 13:52:16 +0545

    deletion in circular LL

--- circularLL_implementation.cpp
+++ circularLL_implementation.cpp
@@ -13,6 +13,19 @@ public:
         this->data = d;
         this->next = NULL;
     }
+
+    // destructor
+    ~Node()
+    {
+        int val = this->data;
+        if (this->next != NULL)
+        {
+            this->next = NULL;
+            delete next;
+        }
+
+        cout << "memoery is free with data" << val << endl;
+    }
 };
 
 void insertNode(Node *&tail, int element, int data)
@@ -39,8 +52,47 @@ void insertNode(Node *&tail, int element, int data)
     }
 }
 
+void deleteNode(Node *&tail, int element)
+{
+    if (tail == NULL)
+    {
+        cout << "LL is empty" << endl;
+    }
+    else
+    {
+        Node *prev = tail;
+        Node *curr = prev->next;
+        while (curr->data != element)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+        prev->next = curr->next;
+
+        // 1 node in linked list
+        if (curr == prev)
+        {
+            tail = NULL;
+        }
+
+        //>=2 linked list
+        if (tail == curr)
+        {
+            tail = prev;
+        }
+        curr->next = NULL;
+        delete curr;
+    }
+}
+
+// traversal in cricular LL
 void display(Node *&tail)
 {
+
+    if (tail == NULL)
+    {
+        cout << "linked list is empty." << endl;
+    }
     Node *temp = tail;
     do
     {
@@ -56,9 +108,16 @@ int main()
     display(tail);
     insertNode(tail, 5, 15);
     display(tail);
-    insertNode(tail, 5, 10);
+    // insertNode(tail, 5, 10);
+    // display(tail);
+    // insertNode(tail, 10, 7);
+    // display(tail);
+    // insertNode(tail, 15, 2);
+    // display(tail);
+
+    // cout << tail->data << endl;
+    deleteNode(tail, 5);
     display(tail);
-    insertNode(tail, 10, 7);
-    display(tail);
+
     return 0;
 }
