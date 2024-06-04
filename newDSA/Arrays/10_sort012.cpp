b762331c99228e184518f5397cc639e7ccca3c86
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-06-04 19:06:09 +0545

    solved using dutch flag algorithm

--- newDSA/Arrays/10_sort012.cpp
+++ newDSA/Arrays/10_sort012.cpp
@@ -1,36 +1,57 @@
 #include <iostream>
 using namespace std;
 
-void sort012(int *arr, int n)
-{
-    int j = n - 1;
-    for (int i = 0; i < n; i++)
-    {
-        // sort 0 & 1
-        while (i < j)
-        {
-            if (arr[i] == 0)
-            {
-                i++;
-            }
-            else if (arr[j] == 2)
-            {
-                j--;
-            }
-            else if (arr[i] != 0 && arr[j] == 0)
-            {
-                swap(arr[i], arr[j]);
-                i++;
-            }
-            else if(arr[i]==1){
-                i++;
-            }
-            else if (arr[i] == 2 && arr[j] == 1)
-            {
-                swap(arr[i], arr[j]);
-                i++;
-                j--;
-            }
+// void sort012(int *arr, int n)
+// {
+//     int j = n - 1;
+//     for (int i = 0; i < n; i++)
+//     {
+//         // sort 0 & 1
+//         while (i < j)
+//         {
+//             if (arr[i] == 0)
+//             {
+//                 i++;
+//             }
+//             else if (arr[j] == 2)
+//             {
+//                 j--;
+//             }
+//             else if (arr[i] != 0 && arr[j] == 0)
+//             {
+//                 swap(arr[i], arr[j]);
+//                 i++;
+//             }
+//             else if(arr[i]==1){
+//                 i++;
+//             }
+//             else if (arr[i] == 2 && arr[j] == 1)
+//             {
+//                 swap(arr[i], arr[j]);
+//                 i++;
+//                 j--;
+//             }
+//         }
+//     }
+// }
+
+// dutch flag algorithm using low , mid and high pointers
+
+void sort012(int *arr ,int n){
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[mid] , arr[low]);
+            mid++;
+            low++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }else{
+            swap(arr[mid] , arr[high]);
+            high--;
         }
     }
 }
