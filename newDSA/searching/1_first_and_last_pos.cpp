ab39b5bcfdf0ba4a0e4b3ad59be3cce043c987e3
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-07-04 03:24:35 +0545

    more optimized solution.

--- newDSA/searching/1_first_and_last_pos.cpp
+++ newDSA/searching/1_first_and_last_pos.cpp
@@ -2,43 +2,99 @@
 #include <vector>
 using namespace std;
 
-pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+// pair<int, int> firstAndLastPosition(vector<int> &arr, int n, int k)
+// {
+//     int start = 0;
+//     int end = n - 1;
+//     int mid = start + (end - start) / 2;
+//     int index, left, right;
+//     while (k != arr[mid] && start <= end)
+//     {
+//         if (k < arr[mid])
+//         {
+//             end = mid - 1;
+//         }
+//         else
+//         {
+//             start = mid + 1;
+//         }
+//         mid = start + (end - start) / 2;
+//     }
+//     if (k == arr[mid])
+//     {
+//         cout<<"hehe_mid: "<<mid<<endl;
+//         index = mid;
+//     }else{
+//         return {-1, -1};
+//     }
+//     // left most index.
+//     while (arr[index] == k)
+//     {
+//         left = index;
+//         index--;
+//     }
+//     index = mid;
+//     while (arr[index] == k)
+//     {
+//         right = index;
+//         index++;
+//     }
+//     return {left, right};
+// }
+
+// optimize
+
+// left occurance.
+int firstOccurance(vector<int> &arr, int n , int k){
+    cout<<endl;
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==k){
+            ans=mid;
+            e=mid-1;
+        }
+        else if(k<arr[mid]){
+            e=mid-1;
+        }
+        else if(k>arr[mid]){
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+
+// right occurance
+int lastOccurance(vector<int> &arr, int n , int k){
+    int s=0, e=n-1;
+    int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        if(arr[mid]==k){
+            ans=mid;
+            s=mid+1;
+        }
+        else if(k<arr[mid]){
+            e=mid-1;
+        }
+        else if(k>arr[mid]){
+            s=mid+1;
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
+
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
 {
-    int start = 0;
-    int end = n - 1;
-    int mid = start + (end - start) / 2;
-    int index, left, right;
-    while (k != arr[mid] && start <= end)
-    {
-        if (k < arr[mid])
-        {
-            end = mid - 1;
-        }
-        else
-        {
-            start = mid + 1;
-        }
-        mid = start + (end - start) / 2;
-    }
-    if (k == arr[mid])
-    {
-        index = mid;
-    }else{
-        return {-1, -1};
-    }
-    // left most index.
-    while (arr[index] == k)
-    {
-        left = index;
-        index--;
-    }
-    index = mid;
-    while (arr[index] == k)
-    {
-        right = index;
-        index++;
-    }
-    return {left, right};
+    pair<int, int> p;
+    p.first= firstOccurance(arr, n ,k);
+    p.second= lastOccurance(arr, n ,k);
+
+    return p;
+
 }
 
 int main()
