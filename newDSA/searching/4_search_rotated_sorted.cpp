0d799245811049b13230511df0524ad1eca25293
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-07-10 23:01:33 +0545

    optimized!!

--- newDSA/searching/4_search_rotated_sorted.cpp
+++ newDSA/searching/4_search_rotated_sorted.cpp
@@ -21,33 +21,75 @@ int getPivot(vector<int>& arr, int n)
     return s;
 }
 
+/* my solution*/
+// int search_rotated_sorted(vector<int>& arr, int n , int k){
+//     int pivot=getPivot(arr, n);
+//     int s=0,e=n-1, mid;
+//     if(k>=arr[0]){
+//         e=pivot;
+//     }else{
+//         s=pivot;
+//     }
+//     mid=s+(e-s)/2;
+//     while(s<=e){
+//         if(k==arr[mid]){
+//             return mid;
+//         }
+//         if(k>arr[mid]){
+//             s=mid+1;
+//         }else{
+//             e=mid-1;
+//         }
+//         mid=s+(e-s)/2;
+//     }
+//     return -1;
+// }
+
+
+
+/* optiimized solution*/
+int binarySearch(vector<int>& arr,int s , int e, int n, int key)
+{
+    int start = s;
+    int end = e;
+    // int mid = (start + end) / 2;
+    /*optimization*/
+    int mid= start+(end-start)/2;
+    while (key != arr[mid] && start <= end)
+    {
+        if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else
+        {
+            end = mid - 1;
+        }
+        mid= start+(end-start)/2;
+    }
+    if (key == arr[mid])
+    {
+        return mid;
+    }
+    else
+    {
+        return -1;
+    }
+}
 
 int search_rotated_sorted(vector<int>& arr, int n , int k){
     int pivot=getPivot(arr, n);
-    int s=0,e=n-1, mid;
-    if(k>=arr[0]){
-        e=pivot-1;
+    if(k>=arr[pivot] && k<=arr[n-1]){
+        return binarySearch(arr, pivot, n-1, n , k);
     }else{
-        s=pivot;
+        return binarySearch(arr, 0, pivot-1 , n ,k);
     }
-    mid=s+(e-s)/2;
-    while(s<=e){
-        if(k==arr[mid]){
-            return mid;
-        }
-        if(k>arr[mid]){
-            s=mid+1;
-        }else{
-            e=mid-1;
-        }
-        mid=s+(e-s)/2;
-    }
-    return -1;
 }
 
+
 int main(){
-    vector<int> arr={12, 15, 18, 2, 4};
-    int index=search_rotated_sorted(arr, 5, 2);
+    vector<int> arr={1};
+    int index=search_rotated_sorted(arr, 1, 1);
     cout<<"this is the index: "<<index<<endl;
     return 0;
 }
