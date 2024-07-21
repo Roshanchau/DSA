04a6b8bb428b9d1064f55f0b8967bc79ce479ca6
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-07-21 22:17:09 +0545

    bubble sort!!

--- newDSA/sorting/2_bubble_sort.cpp
+++ newDSA/sorting/2_bubble_sort.cpp
@@ -1,7 +1,37 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-int main(){
+void bubble_sort(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int k=0;
+        for (int j = k + 1; j < n; j++)
+        {
+            if (arr[j] < arr[k])
+            {
+                swap(arr[j], arr[k]);
+            }
+            k++;
+        }
+    }
+}
 
+void print(vector<int> &arr)
+{
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> arr = {32, 20, 2, 10, 9, 14, 8};
+    print(arr);
+    bubble_sort(arr, 7);
+    print(arr);
     return 0;
 }
