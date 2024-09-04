df20cc289ccf11802caa142407599d23943ae093
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-09-04 14:51:39 +0545

    valid palindrome done!!

--- newDSA/string/5_valid_palindrome.cpp
+++ newDSA/string/5_valid_palindrome.cpp
@@ -1,19 +1,76 @@
 #include <iostream>
 using namespace std;
 
-bool valid_char(char ch){
-    if((ch>='a'&& ch<='z')||(ch>='A'&& ch<='Z')||(ch>='0'&& ch<='9') ){
+bool valid_char(char ch)
+{
+    if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9'))
+    {
         return 1;
-    }else{
+    }
+    else
+    {
         return 0;
     }
 }
 
-int main(){
-    char str[10]={'a' , 'e' , '\0' , 's', 'd', '\0'};
-    cout<<"hehe this is char array "<<str;
+char lowercase(char ch)
+{
+    if((ch>='a' && ch<='z')|| (ch>='0' && ch<='9')){
+        return ch;
+    }else{
+        char temp= ch-'A'+'a';
+        return temp;
+    }
+}
 
-    // string s="heheh\0jdkfjdk\0";
-    // cout<<"this is string "<<s;
+void print (string s){
+    for(int i=0; i<s.length(); i++){
+        cout<<s[i];
+    }
+}
+
+bool palindrome(string str)
+{
+    int n=str.length();
+    int s = 0;
+    string temp;
+    for(int i=0; i<n; i++){
+        if(valid_char(str[i])){
+            temp.push_back(str[i]);
+        }
+    }
+
+    for(int j=0; j<n; j++){
+        temp[j]=lowercase(str[j]);
+    }
+
+    print(temp);
+
+
+    int e=temp.length()-1;
+    while(s<=e){
+        if(temp[s]!=temp[e]){
+            return 0;
+
+        }else{
+            s++;
+            e--;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    string s;
+    cout<<"Enter your string"<<endl;
+    cin>>s;
+
+    bool flag=palindrome(s);
+    if(flag){
+        cout<<"The given string is palindrome"<<endl;
+    }else{
+        cout<<"no"<<endl;
+    }
     return 0;
-} 
+}
