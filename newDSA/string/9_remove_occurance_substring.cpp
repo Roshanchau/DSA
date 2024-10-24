e5608dac3d05ee1a8ad5b4bb6ca5d31c033dbb9b
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-10-24 13:15:40 +0545

    optimized and easier solution.

--- newDSA/string/9_remove_occurance_substring.cpp
+++ newDSA/string/9_remove_occurance_substring.cpp
@@ -1,41 +1,53 @@
 #include <iostream>
 using namespace std;
 
-string removeOccSubStr(string str, string part){
-    cout<<"hello"<<endl;
-    int i=0;
-    int flag=0;
-    int partLen=part.length();
-    cout<<"this is part lenght: "<<partLen<<endl;
-    while(str[i]!='\0'){
-        if(str[i+partLen-1]!=' '){
-        for(int j=i; j<i+partLen; j++){
-            if(str[j]==part[j-i]){
-                cout<<"yo srting ho->"<<str[j]<<endl;
-                flag=1;
-            }else{
-                flag=0;
-                break;
-            }
-        }
-        }
-        cout<<"This is flag->"<<flag<<endl;
-        cout<<"this is the index i->"<<i<<endl;
-        if(flag==1){
-            str.erase(i, partLen);
-            cout<<str<<endl;
-            i=i-(partLen-1);
-            if(i<0){
-                i=0;
-            }
-            cout<<"i lai ghatyepaxi hai=>"<<i<<endl;
-        }else{
-        i++;
-        }
-    }
+// easy solution(yt)
 
+string removeOccSubStr(string str, string part){   
+    while(str.length()!=0 && str.find(part)<str.length()){
+        str.erase(str.find(part) , part.length());
+    }
     return str;
 }
+
+//my solution. 
+
+
+// string removeOccSubStr(string str, string part){
+//     cout<<"hello"<<endl;
+//     int i=0;
+//     int flag=0;
+//     int partLen=part.length();
+//     cout<<"this is part lenght: "<<partLen<<endl;
+//     while(str[i]!='\0'){
+//         if(str[i+partLen-1]!=' '){
+//         for(int j=i; j<i+partLen; j++){
+//             if(str[j]==part[j-i]){
+//                 cout<<"yo srting ho->"<<str[j]<<endl;
+//                 flag=1;
+//             }else{
+//                 flag=0;
+//                 break;
+//             }
+//         }
+//         }
+//         cout<<"This is flag->"<<flag<<endl;
+//         cout<<"this is the index i->"<<i<<endl;
+//         if(flag==1){
+//             str.erase(i, partLen);
+//             cout<<str<<endl;
+//             i=i-(partLen-1);
+//             if(i<0){
+//                 i=0;
+//             }
+//             cout<<"i lai ghatyepaxi hai=>"<<i<<endl;
+//         }else{
+//         i++;
+//         }
+//     }
+
+//     return str;
+// }
 
 void print(string str){
     int i=0;
