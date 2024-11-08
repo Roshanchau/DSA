5f8b8192d662ff76b6b30d1adcc55713e718c712
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-11-08 21:59:27 +0545

    insertion at head and displaying a linkedlist.

--- newDSA/LinkedList/SinglyLL/implementation.cpp
+++ newDSA/LinkedList/SinglyLL/implementation.cpp
@@ -1,22 +1,48 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
+class Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
     // constructor
-    Node(int data){
-        this->data=data;
-        this->next=NULL;
-    }
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    };
 };
 
+void insertAtHead(Node *&head, int d)
+{
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
 
-int main(){
-    Node* node1= new Node(10);
-    cout<<node1->data<<endl;
-    cout<<node1->next<<endl;
+void display(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data<<" ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *node1 = new Node(10);
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    Node *head = node1;
+    display(head);
+
+    insertAtHead(head, 12);
+    display(head);
     return 0;
 }
