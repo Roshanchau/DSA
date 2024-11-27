45710b6dda3b7e007aa4041d0026f0d0b0470de7
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2024-11-27 23:55:55 +0545

    deletion at a position in LL.

--- newDSA/LinkedList/SinglyLL/implementation.cpp
+++ newDSA/LinkedList/SinglyLL/implementation.cpp
@@ -13,7 +13,19 @@ public:
         this->data = data;
         this->next = NULL;
     };
+
+    // destructor
+    ~Node(){
+        int val=this->data;
+        if(this->next!=NULL){
+            delete next;
+            this->next=NULL;
+        }
+        cout<<"mem is free with data"<<val <<endl;
+    }
 };
+
+/*       INSERTION          */
 
 // insertion at head
 void insertAtHead(Node *&head, int d)
@@ -30,8 +42,14 @@ void insertAttail(Node *&tail , int d){
     tail=temp;
 }
 
-// insertion at middle
-void insertAtmiddle(Node *&head , int pos , int d){
+// insertion at middle or any position.
+void insertAtmiddle(Node* &tail , Node *&head , int pos , int d){
+
+    if(pos==1){
+        insertAtHead(head , d);
+        return;
+    }
+
     Node *temp=head;
     int cnt=1;
     while(cnt< pos-1){
@@ -39,11 +57,51 @@ void insertAtmiddle(Node *&head , int pos , int d){
         cnt++;
     }
 
+    if(temp->next==NULL){
+        insertAttail(tail , d);
+        return;
+    }
+
     Node * newNode= new Node(d);
     newNode->next=temp->next;
     temp->next=newNode;
 }
 
+
+/*            DELETION         */
+
+
+// delete node at a position-
+void deletePos(Node * &head , int pos){
+    Node *curr= head;
+    Node* prev=NULL;
+
+    if(pos==1){
+        head=head->next;
+        curr->next=NULL;
+        delete curr;
+        return;
+    }
+    int cnt=1;
+
+
+    while(cnt<pos){
+        if(curr->next==NULL){
+            prev->next=NULL;
+            delete curr;
+            return;
+        }
+       prev=curr;
+       curr=curr->next;
+        cnt++; 
+    }
+
+    prev->next=curr->next;
+    curr->next=NULL;
+    delete curr;
+}
+
+// traverse.
 void display(Node *&head)
 {
     Node *temp = head;
@@ -65,8 +123,6 @@ int main()
     Node *tail=node1;
     display(head);
 
-    // insertAtHead(head, 12);
-    // display(head);
 
     insertAttail(tail, 12);
     display(head);
@@ -76,7 +132,18 @@ int main()
     display(head);
     insertAttail(tail, 30);
     display(head);
-    insertAtmiddle(head, 4, 17);
+    insertAtmiddle(tail , head, 4, 17);
+    display(head);
+    insertAtmiddle(tail , head, 1, 5);
+    display(head);
+    insertAtmiddle(tail , head, 8, 35);
+    display(head);
+
+    deletePos(head, 1);
+    display(head);
+    deletePos(head, 4);
+    display(head);
+    deletePos(head, 6);
     display(head);
     return 0;
 }
