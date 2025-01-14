39a7f280581db797a5342c94b75f2c9a586d2454
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2025-01-14 10:15:50 +0545

    implements binary tree.

---
+++ newDSA/binaryTree/implementation.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+
+class Node{
+    public:
+        int data;
+        Node* left;
+        Node* right;
+
+    Node(int d){
+        this->data=d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+int main(){
+
+    return 0;
+}
