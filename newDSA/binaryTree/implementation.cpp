0812a2098ec42055952e22251ff77ccbc43b4a60
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2025-01-15 14:24:42 +0545

    level order traversal of binary tree, BFS.

--- newDSA/binaryTree/implementation.cpp
+++ newDSA/binaryTree/implementation.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<queue>
 using namespace std;
 
 
@@ -15,7 +16,43 @@ class Node{
     }
 };
 
-int main(){
+// building a tree
+Node* buildTree(Node* root){
+    int data;
+    cout<<"Enter the data"<<endl;
+    cin>>data;
+    root= new Node(data);
 
+    if(data==-1){
+        return NULL;
+    }
+
+    cout<<"Enter the data for left node of"<<data<<endl;
+    root->left=buildTree(root->left);
+    cout<<"Enter the data for right node of"<<data<<endl;
+    root->right=buildTree(root->right);
+    return root;
+}
+
+// levelOrder traversal (BFS)
+void levelOrderTraversal(Node* root){
+    if(!root){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node*curr=q.front();
+        q.pop();
+        cout<<curr->data<<" ";
+        if(curr->left) q.push(curr->left);
+        if(curr->right) q.push(curr->right);
+    }
+}
+
+int main(){
+    Node* root=NULL;
+    root=buildTree(root);
+    levelOrderTraversal(root);
     return 0;
 }
