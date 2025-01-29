190383b1da7e8aa765fb9cf56753b61934981b4c
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2025-01-29 09:40:21 +0545

    traversals and height of tree.

--- newDSA/binaryTree/implementation.cpp
+++ newDSA/binaryTree/implementation.cpp
@@ -72,10 +72,60 @@ void inOrder(Node * root){
     inOrder(root->right);
 }
 
+// preOrder Traversal
+void preOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+
+    preOrder(root->left);
+
+    preOrder(root->right);
+}
+
+// postOrder Traversal
+void postOrder(Node* root){
+    if(root==NULL){
+        return;
+    }
+
+    postOrder(root->left);
+
+    postOrder(root->right);
+
+    cout<<root->data<<" ";
+}
+
+// height of the tree
+int height(Node * root){
+    if(root==NULL){
+        return -1;
+    }
+
+    int lheight= height(root->left);
+    int rheight= height(root->right);
+
+    return max(lheight , rheight)+1;
+}
+
 int main(){
-    Node* root=NULL;
-    root=buildTree(root);
+    // Node* root=NULL;
+    // root=buildTree(root);
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right= new Node(6);
     levelOrderTraversal(root);
     inOrder(root);
+    cout<<endl;
+    preOrder(root);
+    cout<<endl;
+    postOrder(root);
+    cout<<endl;
+    cout<<"height->"<<height(root)<<endl;
     return 0;
 }
